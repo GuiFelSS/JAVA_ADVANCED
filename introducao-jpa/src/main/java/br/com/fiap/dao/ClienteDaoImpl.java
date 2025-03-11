c8795a4aed59306970de91c74df4e40bba51f263
GuiFelSS <guilhermeflp6@gmail.com>
2025-03-10 22:56:09 -0300

    Paramos nessa parte fiz o persistence mas não commitei pois tem meus dados (tenho que descobri como esconder)

--- introducao-jpa/src/main/java/br/com/fiap/dao/ClienteDaoImpl.java
+++ introducao-jpa/src/main/java/br/com/fiap/dao/ClienteDaoImpl.java
@@ -1,4 +1,51 @@
 package br.com.fiap.dao;
 
+import br.com.fiap.entity.Cliente;
+import br.com.fiap.exception.CommitException;
+import br.com.fiap.exception.IdNaoEncontradoException;
+
+import javax.persistence.EntityManager;
+import javax.swing.text.html.parser.Entity;
+
+
 public class ClienteDaoImpl {
+
+    private EntityManager em;
+
+    public ClienteDaoImpl(EntityManager em) {
+        this.em = em;
+    }
+
+    public void cadastrar(Cliente cliente) {
+        em.persist(cliente);
+    }
+
+    public void atulizar(Cliente cliente) throws IdNaoEncontradoException {
+        buscarPorId(cliente.getId());
+        em.merge();
+    }
+
+    public void remover(Cliente cliente) throws IdNaoEncontradoException {
+        Cliente cliente = buscarPorId(id);
+        em.remove(cliente);
+    }
+
+    public Cliente buscarPorId(int id) throws IdNaoEncontradoException {
+        Cliente cliente = em.find(Cliente.class, id);
+        if (cliente == null)
+            throw new IdNaoEncontradoException("Cliente não encontrado!");
+        return cliente;
+    }
+
+    public void commit() throws CommitException {
+        try {
+            em.getTransaction().begin();
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            em.getTransaction().rollback();
+            throw new CommitException();
+        }
+    }
+
 }
--- introducao-jpa/src/main/java/br/com/fiap/entity/Cliente.java
+++ introducao-jpa/src/main/java/br/com/fiap/entity/Cliente.java
@@ -1,4 +1,21 @@
 package br.com.fiap.entity;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="TDS_TB_CLIENTE")
+@SequenceGenerator(name="cliente", sequenceName = "SQ_TDS_TB_CLIENTE", allocationSize = 1)
 public class Cliente {
+
+    @Id
+    @Column(name="id_cliente")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "cliente")
+    private Integer id;
+
+    @Column(name="nm_cliente", nullable = false, length = 100)
+    private String nome;
+
+    //@Column(name="", )
+
+
 }
